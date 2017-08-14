cdbf75a869e5d4a9208dc89c36315e479b0dde8e
Connor O'Shea <connor@connoroshea.com>
2017-08-13 21:58:51 -0400

    Add Scanner

---
+++ src/PointOfService/PointOfService.Hardware.Sample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfService.Hardware.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scanner = new Scanner("STI_USBSCANNER");
+
+            scanner.Start((barcode, symbology) =>
+            {
+                Console.WriteLine(symbology.ToString() + "    " + barcode);
+            });
+
+            Console.WriteLine("Ready...");
+            Console.ReadLine();
+            scanner.Dispose();
+        }
+    }
+}
---
+++ src/PointOfService/PointOfService.Hardware/Scanner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using Microsoft.PointOfService;
+
+namespace PointOfService.Hardware
+{
+    public class Scanner : IDisposable
+    {
+        public Microsoft.PointOfService.Scanner Device { get; }
+
+        public Scanner(string logicalName)
+        {
+            var explorer = new PosExplorer();
+            var device = explorer.GetDevice(DeviceType.Scanner, logicalName);
+
+            Device = explorer.CreateInstance(device) as Microsoft.PointOfService.Scanner;
+        }
+
+        public void Start(Action<string, BarcodeSymbology> action)
+        {
+            Device.DataEvent += (sender, args) =>
+            {
+                var decodedBarcode = Encoding.UTF8.GetString(Device.ScanDataLabel);
+                var symbology = (BarcodeSymbology)Device.ScanDataType;
+
+                action(decodedBarcode, symbology);
+
+                Device.DataEventEnabled = true;
+            };
+
+            Device.Open();
+            Device.Claim(1000);
+            Device.DeviceEnabled = true;
+            Device.DataEventEnabled = true;
+            Device.DecodeData = true;
+        }
+
+        public void Dispose()
+        {
+            Device.DeviceEnabled = false;
+            Device.Release();
+            Device.Close();
+        }
+    }
+}
